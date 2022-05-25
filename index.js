885cfbb7715d1e158cbe283742b7649961280b13
singhshashank25 <shashanksinghranu@gmail.com>
2022-05-26 00:52:46 +0530

    adding options of home page

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+
+
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+  });
+
+
+app.listen(process.env.PORT || 3000, function() {
+console.log('Server started at port 3000')
+})
